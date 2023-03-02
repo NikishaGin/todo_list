18c4284ba501d8f6e1c93398b52cd8c4a041a14a
Nikita Sedov <nikishagin@gmail.com>
2023-03-02 19:11:25 +0300

    создание тудулиста

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -15,7 +15,21 @@ type PropsType = {
 export const TodoList = (props: PropsType) => {
     return (
         <div>
-            <h2>{props.title}</h2>
+            <h3>{props.title}</h3>
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            <ul>
+                {props.tasks.map(el => {
+                    return <li key={el.id}><input type={"checkbox"} checked={el.isDone}/><span>{el.title}</span></li>
+                })}
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
         </div>
     );
 };
